53f9b9815d4cad01ad35740c52fea4cc0f16266d
LucasRibeir0 <ribeir763@gmail.com>
2020-08-24 18:09:25 -0300

    aula
    12

---
+++ meus_exercicios/ex012.js
@@ -0,0 +1,21 @@
+console.log('Digite o código do produto:')
+var produto = 102
+switch (produto){
+    case 100:
+        console.log('Cachorro quente custa R$ 1,70')
+        break
+    case 101:
+        console.log('Bauru simples custa R$ 2,30')
+        break
+    case 102:
+        console.log('Bauru com ovo custa R$ 2,60')
+        break
+    case 103:
+        console.log('O Hamburguer custa R$ 2,40')
+        break
+    case 104:
+        console.log('O Cheeseburguer custa R$ 2,50')
+        break
+    default:
+        console.log('O refrigerante custa R$ 1,00')
+}
